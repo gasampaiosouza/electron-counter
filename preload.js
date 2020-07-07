e0f08b9ab7051ba810dd24822806891e176bc9a0
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-07 20:04:26 -0300

    initial commit

--- preload.js
+++ preload.js
@@ -1,12 +1,12 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+  const button = document.getElementById('counter--button');
+  const counter = document.getElementById('counter--counter');
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+  button.onclick = () => {
+    const currentValue = Number(counter.textContent);
+
+    return (counter.textContent = currentValue + 1);
+  };
+});
